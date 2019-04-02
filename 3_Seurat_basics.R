abe09d674830bb1687988dfbab5ae382dd0e4432
Christopher L Plaisier, PhD <cplaisier@systemsbiology.org>
2019-04-01 21:14:18 -0700

    Updating Basic and the install guide.

--- 3_Seurat_basics.R
+++ 3_Seurat_basics.R
@@ -17,11 +17,16 @@
 ## mention who built it. Thanks. :-)                    ##
 ##########################################################
 
-# Import Seurat single cell analysis software
+# Import Seurat single cell analysis software and dplyr
 library(Seurat)
 library(dplyr)
+
 # Set your working directory
 setwd('C:/Users/cplaisie/Dropbox (ASU)/mctb_single_cell_symposium_data')
+
+########################
+## Load up lung1 data ##
+########################
 
 # Load up data
 lung1_counts = Read10X(data.dir = "Lung1/outs/filtered_gene_bc_matrices/GRCh38/")
@@ -46,28 +51,32 @@ lung1@data[1:20,1:20] #Currently set as raw.data
 # Select mitochondrial genes - mitochondria (MT) have a double membrane, their own genome, and transcriptases.
 # Ratio of MT genes to general cell genes is a great way to assess cell integrity as MT genes will have a harder
 # time escaping the cell if the membrane is compromised.
-mito.genes = grep(pattern = "^MT-", x = rownames(x = lung1@data), value = TRUE)
-length(mito.genes)
-mito.genes
-percent.mito = colSums(lung1@data[mito.genes, ]) / colSums(lung1@data)
-lung1 = AddMetaData(object = lung1, metadata = percent.mito, col.name = "percent.mito")
+mito_genes = grep(pattern = "^MT-", x = rownames(x = lung1@data), value = TRUE)
+length(mito_genes)
+mito_genes
+
+# Calculate percentage of mitochondria
+percent_mito = colSums(lung1@data[mito_genes, ]) / colSums(lung1@data)
+
+# Add to lung1 Seurat object meta data
+lung1 = AddMetaData(object = lung1, metadata = percent_mito, col.name = "percent_mito")
 
 # Violin plot of three parameters
-VlnPlot(object = lung1, features.plot = c("nGene", "nUMI", "percent.mito"), nCol = 3)
+VlnPlot(object = lung1, features.plot = c("nGene", "nUMI", "percent_mito"), nCol = 3)
 
-# Plot of Percent.mito vs. nGene or nUMI
+# Plot of percent_mito vs. nGene or nUMI
 # Best guess data was filtered with nUMI >= 2000
 par(mfrow = c(1,2))
-plot(lung1@meta.data$percent.mito, lung1@meta.data$nUMI, col = rgb(0,0,1,0.5), ylim = c(0,38000), xlim = c(0,0.35))
+plot(lung1@meta.data$percent_mito, lung1@meta.data$nUMI, col = rgb(0,0,1,0.5), ylim = c(0,38000), xlim = c(0,0.35))
 abline(v = 0.1, col = 'red', lwd = 2)
 abline(h = 2000, col = 'red', lwd = 2)
-plot(lung1@meta.data$percent.mito, lung1@meta.data$nGene, col=rgb(0,0,1,0.5), ylim = c(0,6000), xlim = c(0,0.35))
+plot(lung1@meta.data$percent_mito, lung1@meta.data$nGene, col=rgb(0,0,1,0.5), ylim = c(0,6000), xlim = c(0,0.35))
 abline(v = 0.1, col = 'red', lwd = 2)
 
 # Filtering cells
 #  - nUMI >= 2000 UMI
-#  - percent.mito <= 0.1
-lung1 = FilterCells(object = lung1, subset.names = c("nUMI", "percent.mito"), low.thresholds = c(2000, -Inf), high.thresholds = c(Inf, 0.1))
+#  - percent_mito <= 0.1
+lung1 = FilterCells(object = lung1, subset.names = c("nUMI", "percent_mito"), low.thresholds = c(2000, -Inf), high.thresholds = c(Inf, 0.1))
 dim(lung1@data)
 
 # Normalize data
@@ -79,7 +88,7 @@ lung1 = FindVariableGenes(object = lung1)
 length(lung1@var.genes)
 
 # Scale the data and regress out percent of mitochondrial and nUMI effects
-lung1 = ScaleData(object = lung1, vars.to.regress = c('percent.mito','nUMI'), genes.use = lung1@var.genes, model.use = 'negbinom')
+lung1 = ScaleData(object = lung1, vars.to.regress = c('percent_mito','nUMI'), genes.use = lung1@var.genes, model.use = 'negbinom')
 
 # Run PCA analysis
 lung1 = RunPCA(object = lung1, pc.genes = lung1@var.genes, pcs.compute = 40, pcs.print = 1:30, maxit = 500, weight.by.var = FALSE)
@@ -96,26 +105,26 @@ lung2 = CreateSeuratObject(raw.data = lung2_counts)
 # Select mitochondrial genes - mitochondria (MT) have a double membrane, their own genome, and transcriptases.
 # Ratio of MT genes to general cell genes is a great way to assess cell integrity as MT genes will have a harder
 # time escaping the cell if the membrane is compromised.
-mito.genes = grep(pattern = "^MT-", x = rownames(x = lung2@data), value = TRUE)
-percent.mito = colSums(lung2@data[mito.genes, ]) / colSums(lung2@data)
-lung2 = AddMetaData(object = lung2, metadata = percent.mito, col.name = "percent.mito")
+mito_genes = grep(pattern = "^MT-", x = rownames(x = lung2@data), value = TRUE)
+percent_mito = colSums(lung2@data[mito_genes, ]) / colSums(lung2@data)
+lung2 = AddMetaData(object = lung2, metadata = percent_mito, col.name = "percent_mito")
 
 # Violin plot of three parameters
-VlnPlot(object = lung2, features.plot = c("nGene", "nUMI", "percent.mito"), nCol = 3)
+VlnPlot(object = lung2, features.plot = c("nGene", "nUMI", "percent_mito"), nCol = 3)
 
-# Plot of Percent.mito vs. nGene or nUMI
+# Plot of percent_mito vs. nGene or nUMI
 # Best guess data was filtered with nUMI >= 200
 par(mfrow = c(1,2))
-plot(lung2@meta.data$percent.mito, lung2@meta.data$nUMI, col = rgb(0,0,1,0.5), ylim = c(0,38000), xlim = c(0,0.35))
+plot(lung2@meta.data$percent_mito, lung2@meta.data$nUMI, col = rgb(0,0,1,0.5), ylim = c(0,38000), xlim = c(0,0.35))
 abline(v = 0.2, col = 'red', lwd = 2)
 abline(h = 200, col = 'red', lwd = 2)
-plot(lung2@meta.data$percent.mito, lung2@meta.data$nGene, col=rgb(0,0,1,0.5), ylim = c(0,6000), xlim = c(0,0.35))
+plot(lung2@meta.data$percent_mito, lung2@meta.data$nGene, col=rgb(0,0,1,0.5), ylim = c(0,6000), xlim = c(0,0.35))
 abline(v = 0.2, col = 'red', lwd = 2)
 dim(lung2@data)
-lung2 = FilterCells(object = lung2, subset.names = c("nUMI", "percent.mito"), low.thresholds = c(200, -Inf), high.thresholds = c(Inf, 0.2))
+lung2 = FilterCells(object = lung2, subset.names = c("nUMI", "percent_mito"), low.thresholds = c(200, -Inf), high.thresholds = c(Inf, 0.2))
 dim(lung2@data)
 lung2 = NormalizeData(object = lung2)
 lung2 = FindVariableGenes(object = lung2)
-lung2 = ScaleData(object = lung2, vars.to.regress = c('percent.mito','nUMI'), genes.use = lung2@var.genes, model.use = 'negbinom')
+lung2 = ScaleData(object = lung2, vars.to.regress = c('percent_mito','nUMI'), genes.use = lung2@var.genes, model.use = 'negbinom')
 lung2 = RunPCA(object = lung2, pc.genes = lung2@var.genes, pcs.compute = 40, pcs.print = 1:30, maxit = 500, weight.by.var = FALSE)
 PCHeatmap(object = lung2, pc.use = 1:12, cells.use = 300, do.balanced = TRUE, label.columns = FALSE)
