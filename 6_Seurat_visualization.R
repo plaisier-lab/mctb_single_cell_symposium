8f5d424c3f4a2dd63f482343babb7ff7f607102b
Nicholas Banovich <nbanovich@it5570.ad.tgen.org>
2019-04-02 15:56:22 -0700

    Updated clustering script and visualization script

--- 6_Seurat_visualization.R
+++ 6_Seurat_visualization.R
@@ -21,8 +21,30 @@
 # Find all the differntially expressed genes between celltypes - using max.cells.per.ident to speed up for the workshop
 lung_celltype_markers = FindAllMarkers(lung1, max.cells.per.ident = 100)
 
+# This uses dplyr to pull the top 20 DE genes from each cell-type based on absolute value of the avg_logFC
+top20 = lung_celltype_markers %>% group_by(cluster) %>% top_n(20, abs(avg_logFC))
 
-top10 <- lung_celltype_markers %>% group_by(cluster) %>% top_n(10, avg_logFC)
+# Make a heatmap of these genes 
+DoHeatmap(lung1, genes.use = top20$gene)
 
-VlnPlot(lung1, "KRT18")
-DoHeatmap(lung1, genes.use = head(row.names(lung_celltype_markers)))
+# View the heatmap options
+?DoHeatmap
+
+# Remake heatmap with better parameters
+DoHeatmap(lung1, genes.use = top20$gene, slim.col.label = T, title = "Heatmap of DE genes between cell types", group.label.rot = 2)
+
+# How does the heatmap look if we don't use the scaled data
+DoHeatmap(lung1, genes.use = top20$gene, slim.col.label = T, title = "Heatmap of DE genes between cell types", group.label.rot = 2, use.scaled = F)
+
+# Make a violn plot of gene expression for SRGN
+
+VlnPlot(lung1, "SRGN")
+
+# Make a new object with the top 2 DE genes from each celltype
+top2 = lung_celltype_markers %>% group_by(cluster) %>% top_n(2, abs(avg_logFC))
+
+# Make a violin plot of the top2 genes
+VlnPlot(lung1, top2$gene)
+
+# Make FeaturePlots of the top2 genes
+FeaturePlot(lung1, top2$gene)
