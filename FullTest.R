a467dff404a2ae6b1f343dec17294a4c06291e8f
Christopher L Plaisier, PhD <cplaisier@systemsbiology.org>
2019-04-03 20:32:34 -0700

    Made cleaner so we only need to change the setwd.

--- FullTest.R
+++ FullTest.R
@@ -1,31 +1,32 @@
-setwd('C:/Users/cplaisie/Dropbox (ASU)/mctb_single_cell_symposium_data')
+#setwd('C:/Users/cplaisie/Dropbox (ASU)/mctb_single_cell_symposium_data')
+setwd('C:/Users/plais/Dropbox (ASU)/mctb_single_cell_symposium_data')
 
 # Run 2
 #pdf('2_results.R')
-#source('C:/Users/cplaisie/Dropbox (ASU)/mctb_single_cell_symposium/2_GettingAcquaintedWithR.R')
+#source('../mctb_single_cell_symposium/2_GettingAcquaintedWithR.R')
 #dev.off()
 
 # Run 3
 pdf('3_results.pdf')
-source('C:/Users/cplaisie/Dropbox (ASU)/mctb_single_cell_symposium/3_Seurat_basics.R')
+source('../mctb_single_cell_symposium/3_Seurat_basics.R')
 dev.off()
 
 # Run 4
 pdf('4_results.pdf')
-source('C:/Users/cplaisie/Dropbox (ASU)/mctb_single_cell_symposium/4_Seurat_clustering.R')
+source('../mctb_single_cell_symposium/4_Seurat_clustering.R')
 dev.off()
 
 # Run 5
 pdf('5_results.pdf')
-source('C:/Users/cplaisie/Dropbox (ASU)/mctb_single_cell_symposium/5_Seurat_training_classifier_application_new_data.R')
+source('../mctb_single_cell_symposium/5_Seurat_training_classifier_application_new_data.R')
 dev.off()
 
 # Run 7
 pdf('7_results.pdf')
-source('C:/Users/cplaisie/Dropbox (ASU)/mctb_single_cell_symposium/7_Seurat_combining_scRNA_seq_datasets.R')
+source('../mctb_single_cell_symposium/7_Seurat_combining_scRNA_seq_datasets.R')
 dev.off()
 
 # Run 6
 pdf('6_results.pdf')
-source('C:/Users/cplaisie/Dropbox (ASU)/mctb_single_cell_symposium/6_Seurat_visualization.R')
+source('../mctb_single_cell_symposium/6_Seurat_visualization.R')
 dev.off()
