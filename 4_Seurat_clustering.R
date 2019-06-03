3ded9862883165b9927a53278fcf18fd6b103148
Christopher L Plaisier, PhD <plaisier@asu.edu>
2019-06-03 09:41:50 -0700

    Changed clustering to work in Seurat V3.0.

--- 4_Seurat_clustering.R
+++ 4_Seurat_clustering.R
@@ -17,66 +17,85 @@
 ## mention who built it. Thanks. :-)                    ##
 ##########################################################
 
+# Run UMAP using PCs 1:6
+lung1 = RunUMAP(lung1, dims = 1:6)
+DimPlot(lung1, reduction = 'umap')
+
+# Run UMAP using PCs 1:10
+lung1 = RunUMAP(lung1, dims = 1:10)
+DimPlot(lung1, reduction = 'umap')
+
 # Run TSNE using PCs 1:6
-lung1 = RunTSNE(object = lung1, dims.use = 1:6, do.fast = TRUE, perplexity = 30)
-TSNEPlot(lung1)
+lung1 = RunTSNE(lung1, dims = 1:6, do.fast = TRUE, perplexity = 30)
+DimPlot(lung1, reduction = 'tsne')
 
 # Run TSNE using PCs 1:10
-lung1 = RunTSNE(object = lung1, dims.use = 1:10, do.fast = TRUE, perplexity = 30)
-TSNEPlot(lung1)
+lung1 = RunTSNE(lung1, dims = 1:10, do.fast = TRUE, perplexity = 30)
+DimPlot(lung1, reduction = 'tsne')
 
 # Run TSNE using PCs 1:10 with perplexity of 10
-lung1 = RunTSNE(object = lung1, dims.use = 1:10, do.fast = TRUE, perplexity = 10)
-TSNEPlot(lung1)
+lung1 = RunTSNE(lung1, dims = 1:10, do.fast = TRUE, perplexity = 10)
+DimPlot(lung1, reduction = 'tsne')
 
 # Run TSNE using PCs 1:10 with perplexity of 100
-lung1 = RunTSNE(object = lung1, dims.use = 1:10, do.fast = TRUE, perplexity = 100)
-TSNEPlot(lung1)
+lung1 = RunTSNE(lung1, dims = 1:10, do.fast = TRUE, perplexity = 100)
+DimPlot(lung1, reduction = 'tsne')
 
 
 # Find clusters
-lung1 = FindClusters(object = lung1, reduction.type = "pca", dims.use = 1:10, resolution = 0.3, print.output = 0, save.SNN = TRUE)
-TSNEPlot(lung1)
+lung1 = FindNeighbors(lung1, dims = 1:10)
+lung1 = FindClusters(lung1, resolution = 0.3)
+DimPlot(lung1, reduction='umap')
+DimPlot(lung1, reduction='tsne')
 
 # Find clusters at varying resolutions
-lung1 = FindClusters(object = lung1, reduction.type = "pca", resolution = c(0.02, 0.1, .5, 1, 3), print.output = 0, reuse.SNN  = TRUE)
-TSNEPlot(lung1)
+lung1 = FindClusters(lung1, resolution = c(0.02, 0.1 , 0.5, 1, 3))
+DimPlot(lung1, reduction='umap')
+DimPlot(lung1, reduction='tsne')
 
 # View meta data
 head(lung1@meta.data)
 
-# Plot TSNE for differnt resolutions
-TSNEPlot(lung1, group.by = "res.0.02")
-TSNEPlot(lung1, group.by = "res.0.1")
-TSNEPlot(lung1, group.by = "res.0.3")
-TSNEPlot(lung1, group.by = "res.0.5")
-TSNEPlot(lung1, group.by = "res.1")
+# Plot UMAP for differnt resolutions
+DimPlot(lung1, group.by = "RNA_snn_res.0.02", reduction = 'umap')
+DimPlot(lung1, group.by = "RNA_snn_res.0.1", reduction = 'umap')
+DimPlot(lung1, group.by = "RNA_snn_res.0.3", reduction = 'umap')
+DimPlot(lung1, group.by = "RNA_snn_res.0.5", reduction = 'umap')
+DimPlot(lung1, group.by = "RNA_snn_res.1", reduction = 'umap')
+
+# Plot tSNE for differnt resolutions
+DimPlot(lung1, group.by = "RNA_snn_res.0.02", reduction = 'tsne')
+DimPlot(lung1, group.by = "RNA_snn_res.0.1", reduction = 'tsne')
+DimPlot(lung1, group.by = "RNA_snn_res.0.3", reduction = 'tsne')
+DimPlot(lung1, group.by = "RNA_snn_res.0.5", reduction = 'tsne')
+DimPlot(lung1, group.by = "RNA_snn_res.1", reduction = 'tsne')
 
 #Set chosen resolution as ident
-
-lung1 = SetIdent(lung1, ident.use = lung1@meta.data$res.0.1)
+colnames(lung1[[]])
+levels(lung1)
+Idents(lung1) = 'RNA_snn_res.0.1'
+levels(lung1)
 
 # Labeling clusters using known marker genes
-FeaturePlot(lung1, c("EPCAM", "PTPRC", "PECAM1", "PDGFRB")) # Epithelial, Immune, Endothelial, Fibroblast
+FeaturePlot(lung1, features = c("EPCAM", "PTPRC", "PECAM1", "PDGFRB")) # Epithelial, Immune, Endothelial, Fibroblast
 
 # Immune cell types
-FeaturePlot(lung1, c("CD14", "CD3E", "MS4A1")) # Myeloid, T-cell, B-cell
+FeaturePlot(lung1, features = c("CD14", "CD3E", "MS4A1")) # Myeloid, T-cell, B-cell
 
 # Epithelial cell types
-FeaturePlot(lung1, c("MUC5B", "FOXJ1")) # Secretory cells, cilliated cells
+FeaturePlot(lung1, features = c("MUC5B", "FOXJ1")) # Secretory cells, cilliated cells
 
 # Dot plot to see marker gene expression by cluser
-
-DotPlot(lung1, c("PECAM1", "PDGFRB", "MUC5B", "FOXJ1", "CD14"))
+DotPlot(lung1, features = c("PECAM1", "PDGFRB", "MUC5B", "FOXJ1", "CD14"))
 
 # Create new meta data column with cell type annotations using plyr
-current.cluster.ids = c(0, 1, 2, 3, 4, 5, 6)
-new.cluster.ids = c("Fibroblast", "Myeloid", "Myeloid", "Myeloid",
-                     "Secretory", "Cilliated", "Endothelial")
-lung1@meta.data$celltype = plyr::mapvalues(x = lung1@ident, from = current.cluster.ids, to = new.cluster.ids)
-lung1 = SetIdent(lung1, ident.use = lung1@meta.data$celltype)
-TSNEPlot(lung1)
-DotPlot(lung1, c("PECAM1", "PDGFRB", "MUC5B", "FOXJ1", "CD14"))
+new.cluster.ids = c("Fibroblast", "Myeloid", "Myeloid", "Secretory", "Cilliated", "Endothelial", "Fibroblast")
+names(new.cluster.ids) = levels(lung1)
+lung1 = RenameIdents(lung1, new.cluster.ids)
+DimPlot(lung1, reduction = 'umap', label = TRUE, pt.size = 0.5) + NoLegend()
+
+# Dot plot to see marker gene expression by cluser
+DotPlot(lung1, features = c("PECAM1", "PDGFRB", "MUC5B", "FOXJ1", "CD14"))
 
 #Find genes with differntial expression between clusters
 lung_celltype_markers = FindMarkers(lung1, ident.1 = "Cilliated", ident.2 = "Secretory")
